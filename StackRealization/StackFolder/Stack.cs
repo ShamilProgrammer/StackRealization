720d1c81fa371ac1b148cef01edf50dbd7754788
ShamilProgrammer <mirhome2016@gmail.com>
2021-03-30 22:16:15 +0300

    #2 Adding additional methods 'Push' for adding element to the end of the stack 'IsEmpty' - check stack is empty or not

--- StackRealization/StackFolder/Stack.cs
+++ StackRealization/StackFolder/Stack.cs
@@ -9,10 +9,29 @@ namespace StackRealization.StackFolder
     class Stack
     {
         private char[] stack { get; set; }
-        private int length { get; set; }
+        private int length => stack.Length;
         private int MaxLength { get; set; }
+        private int tail { get; set; } 
 
+        public Stack(int maxlength)
+        {
+            MaxLength = maxlength;
+        }
 
+        public void Push(char element)
+        {
+            if (length < MaxLength)
+            {
+                stack.Append(element);
+                tail = stack[stack.Length - 1];
+            }
+            else throw new ArgumentOutOfRangeException(nameof(element));
+        }
 
+        public bool IsEmpty()
+        {
+            if (length == 0) return true;
+            else return false;
+        }
     }
 }
