6a86ba247dd3cb1e7c8599fcd73d159adf73df18
ShamilProgrammer <mirhome2016@gmail.com>
2021-03-30 22:06:59 +0300

    #1 Creating new class 'stack'

---
+++ StackRealization/StackFolder/Stack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackRealization.StackFolder
+{
+    class Stack
+    {
+        private char[] stack { get; set; }
+        private int length { get; set; }
+        private int MaxLength { get; set; }
+
+
+
+    }
+}
